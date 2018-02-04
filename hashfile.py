8849f796acf0b7f4c5e23583944e442928116458
Chris <chris@csfields.com>
2018-02-04 13:04:46 -0500

    Removed double linebreaks

--- hashfile.py
+++ hashfile.py
@@ -3,8 +3,8 @@ from passlib.hash import pbkdf2_sha256
 filename = input("Filepath: ")
 input = open(filename, 'r')
 
-print ("File Contents:\n\n" + input.read() + "\n\n")
+print ("File Contents:\n" + input.read())
 
 hash = pbkdf2_sha256.hash(input.read())
 
-print("Hashing Result: \n\n" + hash)
+print("Hashing Result: \n" + hash)
